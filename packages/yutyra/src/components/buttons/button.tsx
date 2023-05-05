b9cce40f0f2fa51b0c334c5c3142a164e65b4dc7
Takuya Takada <75238542+TakuyaTakada@users.noreply.github.com>
2023-05-05 18:39:08 +0900

    [yutyra] Enable scss

--- packages/yutyra/src/components/buttons/button.tsx
+++ packages/yutyra/src/components/buttons/button.tsx
@@ -1,9 +1,11 @@
 import { ComponentPropsWithoutRef, forwardRef } from "react";
 
+import styles from "./styles.module.scss";
+
 interface ButtonProps extends ComponentPropsWithoutRef<"button"> {}
 
 export const Button = forwardRef<HTMLButtonElement, ButtonProps>(
   ({ children }: ButtonProps, ref) => {
-    return <button ref={ref}>{children}</button>;
+    return <button className={styles.button} ref={ref}>{children}</button>;
   }
 );
