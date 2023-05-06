2463de2ddddb3065dfa359fb848ee7d08bdd19b5
Takuya Takada <75238542+TakuyaTakada@users.noreply.github.com>
2023-05-06 15:34:23 +0900

    [yutyra] Add button stories

--- packages/yutyra/src/components/buttons/button/button.stories.tsx
+++ packages/yutyra/src/components/buttons/button/button.stories.tsx
@@ -10,7 +10,7 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {
+export const Default: Story = {
   args: {
     children: "button",
     variant: "solid",
@@ -18,3 +18,43 @@ export const Primary: Story = {
     color: "primary",
   },
 };
+
+export const Solid = () => (
+  <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+    <Button color="primary" size="sm">
+      primary
+    </Button>
+    <Button color="secondary" size="sm">
+      secondary
+    </Button>
+    <Button color="success" size="sm">
+      success
+    </Button>
+    <Button color="warning" size="sm">
+      warning
+    </Button>
+    <Button color="danger" size="sm">
+      danger
+    </Button>
+  </div>
+);
+
+export const Outline = () => (
+  <div style={{ display: "flex", justifyContent: "space-evenly" }}>
+    <Button variant="outline" color="primary" size="sm">
+      primary
+    </Button>
+    <Button variant="outline" color="secondary" size="sm">
+      secondary
+    </Button>
+    <Button variant="outline" color="success" size="sm">
+      success
+    </Button>
+    <Button variant="outline" color="warning" size="sm">
+      warning
+    </Button>
+    <Button variant="outline" color="danger" size="sm">
+      danger
+    </Button>
+  </div>
+);
